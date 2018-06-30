b3c1d49ef36f5de8a7039bacf72e8ec50ec3eb47
Lawrence <lawrencelim1996@gmail.com>
2018-06-30 16:53:12 +0800

    added SplashActivity on Application launch

--- app/src/main/java/neighbourhoodapp/tnap/com/tnap/SplashActivity.java
+++ app/src/main/java/neighbourhoodapp/tnap/com/tnap/SplashActivity.java
@@ -1,13 +1,17 @@
 package neighbourhoodapp.tnap.com.tnap;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        Intent myIntent = new Intent(this, LoginActivity.class);
+        startActivity(myIntent);
+        finish();
     }
 }
