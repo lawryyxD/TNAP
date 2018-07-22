f27453cd57b3435fbe8bda71aab685f60ac4068c
Phua Yue Jun <whyjayxp@hotmail.com>
2018-07-22 22:21:23 +0800

    Users can now RSVP/cancel RSVP

--- app/src/main/java/neighbourhoodapp/tnap/com/tnap/ShowEventActivity.java
+++ app/src/main/java/neighbourhoodapp/tnap/com/tnap/ShowEventActivity.java
@@ -20,10 +20,12 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
@@ -48,6 +50,8 @@ public class ShowEventActivity extends AppCompatActivity {
     private String cc;
     private int eventid;
     private boolean isAdmin;
+    private boolean isAttending;
+    private String rsvpId;
 
     // Firebase stuff.
     private FirebaseFirestore mDatabase;
@@ -107,18 +111,99 @@ public class ShowEventActivity extends AppCompatActivity {
                     }
             });
         } else {
-            mEventActionButton.setText("RSVP"); // TODO: use R.strings
-            mEventActionButton.setOnClickListener(new View.OnClickListener() {
+            // check if user is attending
+            Query mMyEventQuery = mDatabase.collection("rsvps").whereEqualTo("email", email)
+                    .whereEqualTo("eventid", eventid);
+            mMyEventQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                 @Override
-                public void onClick(View v) {
-                    // TODO: Update RSVP status
-                    Intent intent = new Intent(getParent(), AddEventActivity.class);
-                    intent.putExtra("cc", cc);
-                    intent.putExtra("eventid", eventid);
-                    startActivityForResult(intent, REQUEST_EDIT);
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        QuerySnapshot result = task.getResult();
+                        if (result.isEmpty()) { // not attending
+                            isAttending = false;
+                            mEventActionButton.setText("RSVP"); // TODO: use R.strings
+                        } else {
+                            isAttending = true;
+                            rsvpId = result.getDocuments().get(0).getId();
+                            mEventActionButton.setText("Cancel RSVP"); // TODO: use R.strings
+                        }
+                        mEventActionButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                onRSVP();
+                            }
+                        });
+                    } else {
+                        Log.d("TNAP", "Error getting my events: ", task.getException());
+                    }
                 }
             });
         }
+    }
+
+    private void onRSVP() {
+        // TODO: Add confirmation message before adding/removing RSVP
+        if (isAttending) {
+            // Remove RSVP
+            mDatabase.collection("rsvps").document(rsvpId).delete()
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d("TNAP", "RSVP successfully deleted!");
+                            // update headcount
+                            eventDetails.setHeadcount(eventDetails.getHeadcount() - 1);
+                            mDatabase.collection("events")
+                                    .document(cc + " " + eventid)
+                                    .set(eventDetails);
+                            refreshPage();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("TNAP", "Error deleting RSVP", e);
+                        }
+                    });
+        } else {
+            if (eventDetails.getHeadcount() >= eventDetails.getCapacity()) {
+                // Full already
+                Toast.makeText(this, "This event is full.", Toast.LENGTH_SHORT).show();
+            } else {
+                // Add RSVP
+                Map<String, Object> newRSVP = new HashMap<>();
+                newRSVP.put("cc", cc);
+                newRSVP.put("email", email);
+                newRSVP.put("eventid", eventid);
+                mDatabase.collection("rsvps").add(newRSVP)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("TNAP", "New RSVP written with ID: " + documentReference.getId());
+                                // update headcount
+                                eventDetails.setHeadcount(eventDetails.getHeadcount() + 1);
+                                mDatabase.collection("events")
+                                        .document(cc + " " + eventid)
+                                        .set(eventDetails);
+                                // need success/failure listeners for this one?
+                                refreshPage();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("TNAP", "Error adding RSVP", e);
+                            }
+                        });
+            }
+        }
+
+    }
+
+    private void refreshPage() {
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
     }
 
     private void displayEvent() {
@@ -138,7 +223,6 @@ public class ShowEventActivity extends AppCompatActivity {
         mEndView.setText(new SimpleDateFormat(
                 "dd/MM/yyyy hh:mm a", Locale.US).format(eventDetails.getEnddate()));
     }
-
 
     // back button functionality
     @Override
